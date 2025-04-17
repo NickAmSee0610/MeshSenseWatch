441b90afd68f4499e355eb4c982c2a04deeebd4d
NickAmSee <0610.nico@gmail.com>
2025-04-17 09:15:22 +0000

    Refactoring

--- bridge/src/index.ts
+++ bridge/src/index.ts
@@ -1,15 +1,15 @@
 import WebSocket from 'ws';
-import { XMLParser } from 'fast-xml-parser';
 import config from 'config';
 import axios from 'axios'
 import { LocalStorage } from "node-localstorage";
+import Parser from 'rss-parser'
 
-const url=config.get('api.url')
+const api_url=config.get('api.url')
 const port = config.get('api.port');
 const fetchInterval=config.get('fetchInterval');
 const rss_channel =config.get("rss.channel");
 const localStorage = new LocalStorage('runtimeData');
-
+/* For future implementations
 console.log("Using port %s",port);
 var lastLoad=0;
 var ws = new WebSocket("ws://127.0.0.1:"+port+"/ws");
@@ -23,7 +23,7 @@ ws.on('error', (ws: WebSocket) => {
 ws.on('message', (data:String,isBinary:boolean) => {
   const message = isBinary ? data : data.toString();
   data=JSON.parse(message)
-})
+})*/
 
   
   async function fetchRssFeed(url: string): Promise<void> {
@@ -33,55 +33,30 @@ ws.on('message', (data:String,isBinary:boolean) => {
 
     
     try {  
-
-        const res = await fetch(url);
-              const headerDate = res.headers && res.headers.get('date') ? res.headers.get('date') : 'no response date';     
-              var data = await res.text();
-              data=data.replace("rdf:RDF", "rdfRDF");
-              const parser = new XMLParser();
-              const json=parser.parse(data);
-              let items=null;
-              if(json.rdfRDF==undefined){
-                
-                items=json.rss.channel.item;
-              }else{
-                items=json.rdfRDF.item;
-              }
-              if(items!=null){ 
-              
-              items.forEach((item)=>{
-                let message=item.title;
-                const pubDate=Date.parse(item.pubDate);
-
-                
-                let payload = { message };
-                payload['channel'] = rss_channel;
-
-                //console.log(lastLoadTime.getTime());
-                
-                if(pubDate>lastLoadTime.getTime()){
-                  console.log("New message sent %s",message);
-                  axios.post("http://127.0.0.1:"+port+'/send', payload).then(() => {
-                  
-                  })
-                }
-              
-            
-
-              });
-            }
+      const rssparser=new Parser;
+      rssparser.parseURL(url, function(err, feed) {
+        feed.items.forEach(function(entry) {
+          let message=entry.title;
+          const pubDate=Date.parse(entry.pubDate);
+          let payload = { message };
+          payload['channel'] = rss_channel;
+          if(pubDate>lastLoadTime.getTime()){
+            console.log("New message sent %s",message);
+            axios.post("http://"+api_url+":"+port+'/send', payload).then(() => {});
+          }
+        })
+      })
+        
 
     } catch (error) {
       console.error('Error fetching the RSS feed:', error);
-      throw new Error('Failed to fetch RSS feed');
     }
 
   }
 
 
-
-  setInterval(() => {
-    console.log("Checking new feeds");
+  function loadFeeds(){
+    console.log("%o - Checking new feeds",new Date());
     const feeds=config.get("rss.feeds");
     feeds.forEach((feed)=>{
       console.log("Loading feed: %s",feed.url);
@@ -89,6 +64,10 @@ ws.on('message', (data:String,isBinary:boolean) => {
       
     });
     localStorage.setItem("rss_lastLoadTime",Date.now());
+  }
+
+  setInterval(() => {
+    loadFeeds();
   }, 60000*fetchInterval);
 
   
